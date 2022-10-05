ce38d04ec67260f22018b8291981824f3a7f3b4b
Ngô Tuấn Trường <106718776+ngotruong09@users.noreply.github.com>
2022-10-05 13:29:50 +0700

    .
    .

--- ApplyTest/ExporterSampleHostedService.cs
+++ ApplyTest/ExporterSampleHostedService.cs
@@ -18,7 +18,8 @@ namespace ApplyTest
         private readonly IServiceProvider _serviceProvider;
         private readonly IExporterManager _exporterManager;
 
-        public ExporterSampleHostedService(IAbpApplicationWithExternalServiceProvider application, IServiceProvider serviceProvider, IExporterManager exporterManager)
+        public ExporterSampleHostedService(IAbpApplicationWithExternalServiceProvider application, 
+            IServiceProvider serviceProvider, IExporterManager exporterManager)
         {
             _application = application;
             _serviceProvider = serviceProvider;
@@ -30,21 +31,24 @@ namespace ApplyTest
             _application.Initialize(_serviceProvider);
 
             List<IDictionary<string, object>> _datas = new List<IDictionary<string, object>>();
-            _datas.Add(new Dictionary<string, object>() { { "Name", "truong" }, { "Age", "22" } });
-            _datas.Add(new Dictionary<string, object>() { { "Name", "truong1" }, { "Age", "23" } });
-            _datas.Add(new Dictionary<string, object>() { { "Name", "truong2" }, { "Age", "24" } });
-            _datas.Add(new Dictionary<string, object>() { { "Name", "truong3" }, { "Age", "25" } });
-
+            _datas.Add(new Dictionary<string, object>() { { "Name", "A" }, { "Age", "22" } });
+            _datas.Add(new Dictionary<string, object>() { { "Name", "B" }, { "Age", "23" } });
+            _datas.Add(new Dictionary<string, object>() { { "Name", "C" }, { "Age", "24" } });
+            _datas.Add(new Dictionary<string, object>() { { "Name", "D" }, { "Age", "25" } });
 
             // Save csv
-            using (var stream = await _exporterManager.GetStreamDocument(ExportCsvType.GetExportType(), _datas, new OptionCsv { TempFolderPath = @"d:\MyGit\save2\save" }))
+            using (var stream = 
+                await _exporterManager.GetStreamDocument(
+                    ExportCsvType.GetExportType(), _datas, new OptionCsv { TempFolderPath = @"d:\MyGit\save2\save" }))
             using (var fileStream = new FileStream(@"d:\MyGit\save2\test.csv", FileMode.Create, FileAccess.Write))
             {
                 stream.WriteTo(fileStream);
             }
 
             // Save excel
-            using (var stream = await _exporterManager.GetStreamDocument(ExportExcelType.GetExportType(), _datas, new OptionExcel { NumberRowPerSheet = 2 }))
+            using (var stream = 
+                await _exporterManager.GetStreamDocument(
+                    ExportExcelType.GetExportType(), _datas, new OptionExcel { NumberRowPerSheet = 2 }))
             using (var fileStream = new FileStream(@"d:\MyGit\save2\test.xlsx", FileMode.Create, FileAccess.Write))
             {
                 stream.WriteTo(fileStream);
