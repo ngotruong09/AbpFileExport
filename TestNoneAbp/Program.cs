8183ab136786023cc068ab373688912cf021f34f
Ngô Tuấn Trường <106718776+ngotruong09@users.noreply.github.com>
2022-10-17 16:57:33 +0700

    .
    .

---
+++ TestNoneAbp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TestNoneAbp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var collections = new ServiceCollection();
+            collections.BuildServiceProvider();
+
+        }
+    }
+}
